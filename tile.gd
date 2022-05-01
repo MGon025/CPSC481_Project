bba9ad309be74712e24b13f8746eb4e6a449f2d5
Mark Gonzalez <mgon025@csu.fullerton.edu>
2022-04-30 22:13:54 -0700

    mark toggled on release
    old: visibility toggled on click press
    new: visibility toggled on click release

--- tile.gd
+++ tile.gd
@@ -3,12 +3,25 @@ extends Area2D
 signal mark_clicked
 
 var enabled = true
+var _selected = false
+var _click_saved = false
 
 
 func _ready():
-	if not is_connected("input_event", self, "_on_click"):
-		# warning-ignore:return_value_discarded
-		connect("input_event", self, "_on_click")
+	# warning-ignore:return_value_discarded
+	connect("input_event", self, "_on_click")
+	# warning-ignore:return_value_discarded
+	connect("mouse_exited", self, "_on_mouse_exit")
+	# warning-ignore:return_value_discarded
+	connect("mouse_entered", self, "_on_mouse_enter")
+
+
+func _input(event):
+	# forget that a tile was selected if mouse is released outside of the tile
+	if not (event is InputEventMouseButton):
+		return
+	if not event.is_pressed() and _click_saved:
+		_click_saved = false
 
 
 func turn_on(circle: bool):
@@ -26,9 +39,25 @@ func turn_off():
 # called when a tile is left-clicked
 func _on_click(_viewport, event, _shape_idx):
 	if not (event is InputEventMouseButton) or\
-			not event.is_pressed() or\
 			not enabled:
 		return
-	enabled = false
-	emit_signal("mark_clicked")
-	print(self.name + " clicked & disabled!")
+	if event.is_pressed():
+		_selected = true
+		return
+	elif _selected:
+		_selected = false
+		enabled = false
+		turn_on(true)
+		emit_signal("mark_clicked")
+		print(self.name + " clicked & disabled!")
+
+
+func _on_mouse_exit():
+	_click_saved = true
+	_selected = false
+
+
+func _on_mouse_enter():
+	if _click_saved:
+		_selected = true
+		_click_saved = false
